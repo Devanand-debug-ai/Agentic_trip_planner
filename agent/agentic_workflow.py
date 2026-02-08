824dbddc0312436233ed61a045571e1a843f64e9
Devanand-debug-ai <devanandksj@gmail.com>
2026-02-08 21:10:25 +0530

    started coding for loading the models

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,12 @@
+class GraphBuilder():
+    def __init__(self):
+        pass
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+    def __call__(self):
+        pass
+    
