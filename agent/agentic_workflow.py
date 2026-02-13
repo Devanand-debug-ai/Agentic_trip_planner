00fe1ae276a6e7f247e17a11947200da9b454bf5
Devanand-debug-ai <devanandksj@gmail.com>
2026-02-13 21:28:15 +0530

    add an tool and also added the tools to the agentic workflow

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,4 +1,4 @@
-from urllib import response
+
 from utils.model_loader import ModelLoader
 from prompt_library.prompt import SYSTEM_PROMPT
 from langgraph.graph import StateGraph, MessagesState, END, START
@@ -8,28 +8,36 @@ from tools.place_search_tool import PlaceSearchTool
 from tools.expense_calculator_tool import CalculatorTool
 from tools.currency_conversion_tool import CurrencyConverterTool
 
-
-
-
 class GraphBuilder():
-    def __init__(self):
+    def __init__(self,model_provider: str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        
+        self.tools = []
+        
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyConverterTool()
+        
+        self.tools.extend([* self.weather_tools.weather_tool_list, 
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        
+        self.graph = None
+        
         self.system_prompt = SYSTEM_PROMPT
-        self.tools = [
-            WeatherInfoTool(),
-            PlaceSearchTool(),
-            CalculatorTool(),
-            CurrencyConverterTool()
-        ]
-        self.model_loader = ModelLoader()
-        self.llm_with_tools = self.model_loader.get_model_with_tools(self.tools)
-
+    
+    
     def agent_function(self,state: MessagesState):
         """Main agent function"""
         user_question = state["messages"]
         input_question = [self.system_prompt] + user_question
         response = self.llm_with_tools.invoke(input_question)
         return {"messages": [response]}
-
     def build_graph(self):
         graph_builder=StateGraph(MessagesState)
         graph_builder.add_node("agent", self.agent_function)
@@ -41,7 +49,5 @@ class GraphBuilder():
         self.graph = graph_builder.compile()
         return self.graph
         
-
     def __call__(self):
         return self.build_graph()
-    
